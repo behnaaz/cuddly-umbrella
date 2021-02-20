6dfe45b991acef09f0ed85f224e351b40f4d63a0
Behnaz <behnaz.changizi@gmail.com>
2021-02-20 18:06:27 +0000

    Organize output

---
+++ src/whatsapp.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const url = "https://web.whatsapp.com/";
+  const outname = "output/"+url.replace("https://", "").replace(/\//g, "").replace(/\./g, "-");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, {waitUntil: 'networkidle2'});
+  await page.pdf({path: outname+'.pdf', format: 'A4'});
+  await page.screenshot({ path: outname+'.png' });
+  c = await page.cookies();
+  console.log(JSON.stringify(c));
+  await browser.close();
+})();
+
